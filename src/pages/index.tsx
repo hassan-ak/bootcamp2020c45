bd6afa4e9bb5b13021b52bf8843a1b337b6df779
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-27 16:45:33 +0500

    Step03:- Update product with multiple properties and use discounts
    Steps:-
    	1:- Updated src/pages/index.tsx
    	2:- Create a discount code on web and use it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,10 @@ export default function Home() {
         data-item-url='/'
         data-item-image='https://cdn.shopify.com/s/files/1/0104/5757/9583/products/OSCO-Men-Dress-Shoes-Men-Formal-Shoes-Leather-Luxury-Fashion-Wedding-Shoes-Men-Business-Casual-Oxford.jpg?v=1575045878'
         data-item-name='Formal Shoe'
+        data-item-custom1-name='Size'
+        data-item-custom1-options='8|9[+5]|10[+10]'
+        data-item-custom2-name='Info'
+        data-item-custom2-type='checkbox'
       >
         CheckOut
       </button>
